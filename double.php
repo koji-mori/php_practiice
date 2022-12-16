4977e1ff0475c6f0c095148ca34c4dd5cfdf182e
koji <koko427moko@gmail.com>
2022-12-16 13:05:06 +0000

    add double.php

---
+++ double.php
@@ -0,0 +1,8 @@
+<?php 
+
+function double($num) {
+    return $num * 2;
+}
+
+
+echo double(5) . PHP_EOL;
---
+++ php_practice.php
@@ -0,0 +1,35 @@
+
+<?php 
+
+function  number(double) {
+    $double *=2;
+}
+
+nunber()
+
+
+
+
+function add($a, $b){
+    
+    $result=$a + $b
+ //処理したい内容
+ }
+ 
+ return $result
+ 
+ 
+ 
+ 
+ function max_array($arr){
+  // とりあえず配列の最初の要素を一番大きい値とする
+  $max_number = $arr[0];
+  foreach($arr as $a){
+    // ここで配列の中の1番大きい値を探したい
+    arsort
+  }
+
+  return $max_number;
+}
+
+
